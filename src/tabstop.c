32fa030d7996a50edff443a0dea292694a053ce8
Marco-Chen-06 <marcochen.work@gmail.com>
2025-09-16 21:19:06 -0400

    handled command line arguments for tabstop

--- src/tabstop.c
+++ src/tabstop.c
@@ -1,5 +1,41 @@
-#include "mylib.h"
+#include "tabstop.h"
+
+// simply opens standard input, and reads, then closes the file
+int process_stdin() {
+	// check if file opening was unsuccessful
+	if (myfdopen(0, "w") == NULL) {
+		return -1;
+	}
+
+
+
+}
 
 int main(int argc, char* argv[]) {
-	printf("You have entered %d arguments: \n", argc);
+	int opt;
+	while ((opt = getopt(argc, argv, "o:")) != -1) {
+		switch(opt) {
+			case 'o':
+				printf("found output with %s\n", optarg);
+				break;
+			case '?':
+				return 255;
+		}
+	}
+
+	// make sure there isn't more than one non-option argument
+	if ((argc - optind) > 1) {
+		return -1;
+	}
+
+	if ((argc - optind) == 1) {
+		process_stdin();
+	}
+
+	for (int i = optind; i < argc; i++) {
+		printf("argcount: %d\n", argc);
+		printf("optind: %d\n", optind);
+		// printf("extra argument: %s\n", argv[i]);
+	}
+	return 0;
 }
